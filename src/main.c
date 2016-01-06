751459b094a42b7be7ca6b34eb3d3f142f3e6aca
Marion GUILLON <mguillon@e2r6p12.42.fr>
2016-01-06 18:32:53 +0100

    .h

--- src/main.c
+++ src/main.c
@@ -6,8 +6,26 @@
 /*   By: mguillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 14:49:52 by mguillon          #+#    #+#             */
-/*   Updated: 2016/01/06 14:49:54 by mguillon         ###   ########.fr       */
+/*   Updated: 2016/01/06 18:32:35 by mguillon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "rtv1.h"
 
+int		main(int argc, char **argv)
+{
+	t_env e;
+
+	argv = (char **)argv;
+	if (argc != 1)
+	{
+		ft_putendl("Hey buddy, there was no need to give me any params, but 
+				thanks anyway !"); 
+	}
+
+	e.mlx = mlx_init();
+	e.win = mlx_new_window(e.mlx, WIN_WIDTH, WIN_HEIGHT, "RTV1");	
+	ft_initialize(&e);
+
+	return (0);
+}
