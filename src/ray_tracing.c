d8ab497cf37dfd0e8c90efb15614eefca4dcd548
Charles HUANG <chuang@e3r8p5.42.fr>
2016-01-12 16:57:33 +0100

    added function in vector, switch ray casting to barbare

--- src/ray_tracing.c
+++ src/ray_tracing.c
@@ -1,4 +1,4 @@
-/ ************************************************************************** */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ray_tracing.c                                      :+:      :+:    :+:   */
@@ -6,20 +6,36 @@
 /*   By: chuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:01:28 by chuang            #+#    #+#             */
-/*   Updated: 2016/01/06 17:43:21 by chuang           ###   ########.fr       */
+/*   Updated: 2016/01/12 16:55:48 by chuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define FOV 90
 
-t_vect		pxl_vect(int x, int y, env *e)
+void		ft_render(env *e)
 {
-	double		degree;
-	t_vect		ray;
+	t_vector		ray;
+	float			degree;
+	float			invW;
+	float			invH;
+	float			ratio;
+	float			angle;
+	int				x;
+	int				y;
 
-	degree = 
-	ray.x = d_cos(degree) * x + d_sin(degree) * y;
-	ray.y = d_sin(degree) * x + d_cos(degree) * y;
-	ray.z = 0; // need to find a formula that work even if your cam wach on Z axis
-	return (ray);
+	invW = 1 / (float) WINDOW_W;
+	invH = 1 / (float) WINDOW_H;
+	ratio = WINDOW_W / (float) WIDOW_H;
+	angle = tanf(PI *0.5f * FOV/180.);
+		y = -1;
+	while(++y < WINDOW_H)
+	{
+		x = -1;
+		while (++x < WINDOW_W)
+			ray.x = (2 *((x) * invW) - 1) * angle * ratio;
+			ray.y = (1 - 2 * ((y) * invH)) * angle;
+			ray.z = -1;
+			ray = unit_vector(ray);
+			r
+	}
 }
--- src/vector.c
+++ src/vector.c
@@ -6,14 +6,32 @@
 /*   By: chuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/11 17:31:14 by chuang            #+#    #+#             */
-/*   Updated: 2016/01/11 19:03:41 by mguillon         ###   ########.fr       */
+/*   Updated: 2016/01/12 16:47:03 by chuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-double		dotproduct(t_vect a, t_vect b)
+float		dotproduct(t_vect a, t_vect b)
 {
-	double		ret;
+	float		ret;
 
 	ret = a.x * b.x + a.y * b.y + a.z * b.z;
 	return (ret);
+}
+
+float		norm_vector(t_vect a)
+{
+	return(sqrt(a.x * a.x + a.y * a.y + a.z * a.z));
+}
+
+t_vector	unit_vector(t_vector v)
+{
+	float norm;
+
+	if ((norm = norm_vector(v)) != 1)
+	{
+		v.x /= norm;
+		v.y /= norm;
+		v.z /= norm;
+	}
+	return (v);
 }
