b649ba76a67361dfca3f1411c7da811305344314
agadiffe <agadiffe@student.42.fr>
2016-01-13 11:34:16 +0100

    some fix protect

--- libft/srcs/ft_strjoin.c
+++ libft/srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mguillon <mguillon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 16:00:11 by mguillon          #+#    #+#             */
-/*   Updated: 2014/11/13 14:37:08 by mguillon         ###   ########.fr       */
+/*   Updated: 2016/01/13 11:32:12 by agadiffe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (s1 && s2)
 	{
 		c = ft_strnew(ft_strlen(s1) + (ft_strlen(s2)));
+		if (c == NULL)
+			return (NULL);
 		ft_strcpy(c, s1);
 		c = ft_strcat((char *)c, s2);
 		return (c);
