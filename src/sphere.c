9ef789fea5fef7aeebb6be8eadda2ab3358b6ef4
TreeFell <chuang@student.42.fr>
2016-01-14 18:44:57 +0100

    sphere intersection fixed

--- src/sphere.c
+++ src/sphere.c
@@ -6,7 +6,7 @@
 /*   By: chuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/12 19:07:52 by chuang            #+#    #+#             */
-/*   Updated: 2016/01/13 17:33:18 by chuang           ###   ########.fr       */
+/*   Updated: 2016/01/14 18:44:19 by chuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,30 @@ float		sq_segment(t_vector a)
 }
 
 // need a check
-float		inter_sphere(t_vect cam, t_vect ray, item object)
+float		inter_sphere(t_vector cam, t_vector ray, item object)
 {
-	float		w;
-	float		dist_sq_obj_ray;
-	t_vect		s_ray;
-	t_vect		orig;
+	float			w;
+	float			t;
+	float			d;
+	float			dist_sq_obj_ray;
+	t_vector		s_ray;
+	t_vector		orig;
 
 	w = 1;
-	s_ray = ray;
+	s_ray = set_vector(0,0,0);
 	orig = sub_vector(object->pos, cam);
-	while ( w < MAX_VIS)
+	while ( w < MAX_VISION)
 	{
 		s_ray = add_vector(s_ray, ray);
 		dist_sq_obj_ray = sq_segment((orig)) - sq_segment(vect_mult(ray, w));
-		if (dist_obj_ray < (object->radius * object->radius))
+		if (dist_sq_obj_ray < (object->radius * object->radius))
 		{
-			d = sqrt(object->radius * object->radius - dist_obj_ray);
+			d = sqrt((object->radius * object->radius) - (dist_sq_obj_ray * dist_sq_obj_ray));
 			if (s_ray + d > 0 && s_ray + d < s_ray - d)
 				t = s_ray + d;
 			else if ( s_ray - d > 0 && s_ray - d < s_ray + d)
 				t = s_ray - d;
-			w = MAX_VIS;
+			w = MAX_VISION;
 		}
 		w++;
 	}
