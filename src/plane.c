4ef74daac070a4e4acb7876c650eb92a982bb724
treeFell <chuang@student.42.fr>
2016-01-26 15:20:23 +0100

    1st plane formula

--- src/plane.c
+++ src/plane.c
@@ -6,7 +6,7 @@
 /*   By: mguillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 15:57:57 by mguillon          #+#    #+#             */
-/*   Updated: 2016/01/22 16:14:50 by mguillon         ###   ########.fr       */
+/*   Updated: 2016/01/26 15:19:49 by chuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 
 float	inter_plane(t_cam cam, t_vector ray, t_plane obj)
 {
-	t_vector dist;
-
-	dist = sub_vector(cam.pos, obj.pos);
-	
+	t = (dotpro_vector(obj.normal, obj.pos)+ obj.constant)
+			/ (dotpro_vector(obj.normal, ray));
+	return (t);
 }
