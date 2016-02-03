6a6351d30d0224b5b96c81f05c5d24c31b63cde0
treeFell <chuang@e3r6p4.42.fr>
2016-02-03 17:45:00 +0100

    plane format

--- src/plane.c
+++ src/plane.c
@@ -6,7 +6,7 @@
 /*   By: mguillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 15:57:57 by mguillon          #+#    #+#             */
-/*   Updated: 2016/02/03 12:58:08 by chuang           ###   ########.fr       */
+/*   Updated: 2016/02/03 13:43:49 by chuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ t_object	set_plane(t_vector pos, t_vector normal)
 	return (plane);
 }
 
-float		inter_plane(t_cam cam, t_vector ray, t_object obj)
+float		inter_plane(t_vector cam_pos, t_vector ray, t_object obj)
 {
 	float		t;
 
-	t = -((dotpro_vector(obj.normal, sub_vector(cam.pos, obj.pos)) + obj.constant)
+	t = -((dotpro_vector(obj.normal, sub_vector(cam_pos, obj.pos)) + obj.constant)
 			/ (dotpro_vector(obj.normal, ray)));
 	return (t);
 }
