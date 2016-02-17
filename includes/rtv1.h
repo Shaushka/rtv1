32665396bbea602eb7e52eeedb9adf95112a6b52
Maxime GRAS <mgras@e2r3p6.42.fr>
2016-02-17 14:14:01 +0100

    interface update

--- includes/rtv1.h
+++ includes/rtv1.h
@@ -6,7 +6,7 @@
 /*   By: mgras <mgras@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/06 17:59:10 by mguillon          #+#    #+#             */
-/*   Updated: 2016/02/17 13:04:54 by mgras            ###   ########.fr       */
+/*   Updated: 2016/02/17 14:12:23 by mgras            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,16 @@
 # include <sys/uio.h>
 # include "../libft/includes/libft.h"
 
-# define SCREEN_W		1280
-# define SCREEN_H		960
+# define SCREEN_W			1280
+# define SCREEN_H			960
 
-# define INTER_W		SCREEN_W / 5
-# define INTER_H		SCREEN_H / 5
+# define INTER_W			SCREEN_W / 5
+# define INTER_H			SCREEN_H / 5
 
-# define AMBIANT		0.4
+# define AMBIANT			0.4
 # define MAX_VISION(h)		(3570 + sqrt((h)))
-# define PI			3.14159265
-# define RATIO		(PI / 180)
+# define PI					3.14159265
+# define RATIO				(PI / 180)
 
 # define BUF_SIZE 10
 
@@ -58,18 +58,18 @@ typedef	enum {
 
 typedef	struct		s_object
 {
-	void				*next;
-	t_type				type;
-	t_color				color;
-	t_vector			pos;
-	t_vector			dir;//cylinder cone
-	t_vector			normal;//plan
-	float				radius;//sphere cone cylindre
-	float				height;//cone cylindre
-	float				shine;//brillance
-	float				reflect;//reflexion
-	float				checkered;//dallage_carreaux
-}						t_object;
+	void			*next;
+	t_type			type;
+	t_color			color;
+	t_vector		pos;
+	t_vector		dir;//cylinder cone
+	t_vector		normal;//plan
+	float			radius;//sphere cone cylindre
+	float			height;//cone cylindre
+	float			shine;//brillance
+	float			reflect;//reflexion
+	float			checkered;//dallage_carreaux
+}					t_object;
 
 typedef struct		s_light
 {
@@ -108,30 +108,30 @@ typedef struct		s_img
 
 typedef struct		s_mlx_init
 {
-	t_img		img;
-	void		*mlx;
-	void		*win;
+	t_img			img;
+	void			*mlx;
+	void			*win;
 }					t_mlx_init;
 
-typedef struct	s_keyring
+typedef struct		s_keyring
 {
-	double		mouse_x;
-	double		mouse_y;
-	int			selected_light;
-	int			selected_obj;
-	int			mode;
-	int			visible;
-	t_mlx_init	interface;
-}				t_keyring;
+	double			mouse_x;
+	double			mouse_y;
+	int				selected_light;
+	int				selected_obj;
+	int				mode;
+	t_cam			cam_inc;
+	t_mlx_init		interface;
+}					t_keyring;
 
-typedef struct	s_env
+typedef struct		s_env
 {
-	t_mlx_init	mlx_init;
-	t_scene		*scene;
-	t_light		*lights;
-	t_cam		cam;
-	t_keyring	key;
-}				t_env;
+	t_mlx_init		mlx_init;
+	t_scene			*scene;
+	t_light			*lights;
+	t_cam			cam;
+	t_keyring		key;
+}					t_env;
 
 typedef struct		s_node
 {
@@ -150,45 +150,45 @@ typedef struct		s_parse
 /*
 **	main.c
 */
-void			ft_initialize(t_env *e);
+void				ft_initialize(t_env *e);
 
 /*
 **	initialization.c
 */
-void			init_keyring(t_env *e);
-void			ft_exit(char *str, int n);
-void			quit_and_free(t_env *e);
-void			init_env(t_env *e, char *av);
-int				expose_hook(t_env *e);
-void			init_and_draw(t_env *e, char *av);
-void			init_obj(t_object *node);
-void			init_scene(t_env *e);
-void			new_img_in_old_env(t_env *e, t_env *old);
+void				init_keyring(t_env *e);
+void				ft_exit(char *str, int n);
+void				quit_and_free(t_env *e);
+void				init_env(t_env *e, char *av);
+int					expose_hook(t_env *e);
+void				init_and_draw(t_env *e, char *av);
+void				init_obj(t_object *node);
+void				init_scene(t_env *e);
+void				new_img_in_old_env(t_env *e, t_env *old);
 
 /*
 **	premade_objects.c
 */
-void			set_list_obj(t_env *e);
+void				set_list_obj(t_env *e);
 
 /*
 **	ray_tracing.c
 */
-void			ft_render(t_env *e);
+void				ft_render(t_env *e);
 
 /*
 **	hook_functions.c
 */
-int				expose_hook(t_env *e);
-int				key_press_hook(int keycode, t_env *e);
+int					expose_hook(t_env *e);
+int					key_press_hook(int keycode, t_env *e);
 
 /*
 **	graphics_tools.c
 */
-void			put_pixel_to_img(t_env *e, int addr, t_color color);
-void			set_color_from_img(t_color *color, char *data, int addr);
-void			set_color_from_rgb(t_color *color, int r, int g, int b);
-void			get_true_color_value(t_color *color);
-void			blend_color(t_color *c, t_color c1, t_color c2, double percent);
+void				put_pixel_to_img(t_env *e, int addr, t_color color);
+void				set_color_from_img(t_color *color, char *data, int addr);
+void				set_color_from_rgb(t_color *color, int r, int g, int b);
+void				get_true_color_value(t_color *color);
+void				blend_color(t_color *c, t_color c1, t_color c2, double percent);
 
 /*
 **	----------> USER INPUT SYSTEM <-----
@@ -196,63 +196,63 @@ void			blend_color(t_color *c, t_color c1, t_color c2, double percent);
 /*
 **	create_scene.c
 */
-void			create_scene(t_env *e);
+void				create_scene(t_env *e);
 
 /*
 **	add_shape.c
 */
-void			input_sphere(t_env *e);
-void			input_plane(t_env *e);
-void			input_cylinder(t_env *e);
-void			input_cone(t_env *e);
+void				input_sphere(t_env *e);
+void				input_plane(t_env *e);
+void				input_cylinder(t_env *e);
+void				input_cone(t_env *e);
 
 /*
 **	assign_color.c
 */
-void			color(t_color *color);
-void			color_choice(t_color *color);
+void				color(t_color *color);
+void				color_choice(t_color *color);
 
 /*
 **	create_object.c
 */
-t_object		*create_object(t_env *e);
+t_object			*create_object(t_env *e);
 
 /*
 **	infos_shapes.c
 */
-void			radius(t_object *node);
-void			height(t_object *node);
-void			constant(t_object *node);
+void				radius(t_object *node);
+void				height(t_object *node);
+void				constant(t_object *node);
 /*
 ** int			count_objects(t_env *e);
 */
 /*
 **	input_normal.c
 */
-void			assign_normal(t_object *node, char *str);//static ?
-void			normal(t_object *node);
+void				assign_normal(t_object *node, char *str);//static ?
+void				normal(t_object *node);
 
 /*
 **	input_vectors_clean.c
 */
-char			**remove_spaces(char *str, int i, char **clean);//static ?
-void			separators(char *str);//static ?
-char			*no_more_spaces(char *str);
+char				**remove_spaces(char *str, int i, char **clean);//static ?
+void				separators(char *str);//static ?
+char				*no_more_spaces(char *str);
 
 /*
 **	input_verification.c
 */
-int				is_slashes_and_digits(char *str);//static ?
-int				is_vector(char *str);//static ?
-int				correct_input(char *str);
+int					is_slashes_and_digits(char *str);//static ?
+int					is_vector(char *str);//static ?
+int					correct_input(char *str);
 
 /*
 **	input_position.c
 */
-char			*cut_pos(char *str, int i);
-int				check_values(char *str, int min, int max);
-void			assign_pos(t_object *node, char *str);
-void			position(t_object *node);
+char				*cut_pos(char *str, int i);
+int					check_values(char *str, int min, int max);
+void				assign_pos(t_object *node, char *str);
+void				position(t_object *node);
 
 /*
 **	----------> GEOMETRY <-----
@@ -260,9 +260,9 @@ void			position(t_object *node);
 /*
 **	trigo.c
 */
-float			d_cos(float angle);
-float			d_sin(float angle);
-float			d_acos(float value);
+float				d_cos(float angle);
+float				d_sin(float angle);
+float				d_acos(float value);
 
 /*
 **>----------> INTERSECTION <-----
@@ -271,30 +271,30 @@ float			d_acos(float value);
 /*
 **	sphere.c
 */
-t_object		set_sphere(t_vector pos, float radius);
-float			inter_sphere(t_vector cam_pos, t_vector ray, t_object sphere);
-t_vector		normal_sphere(t_vector cam, t_object obj, t_vector ray);
+t_object			set_sphere(t_vector pos, float radius);
+float				inter_sphere(t_vector cam_pos, t_vector ray, t_object sphere);
+t_vector			normal_sphere(t_vector cam, t_object obj, t_vector ray);
 
 /*
 **	plane.c
 */
-t_object		set_plane(t_vector pos, t_vector normal);
-float			inter_plane(t_vector cam_pos, t_vector ray, t_object plane);
-t_vector		normal_plane(t_object obj, t_vector ray);
+t_object			set_plane(t_vector pos, t_vector normal);
+float				inter_plane(t_vector cam_pos, t_vector ray, t_object plane);
+t_vector			normal_plane(t_object obj, t_vector ray);
 
 /*
 **	cylinder.c
 */
-t_object		set_cylinder(t_vector pos, t_vector dir, float r, float h);
-float			inter_cylinder(t_vector cam_pos, t_vector ray, t_object obj);
-t_vector		normal_cylinder(t_vector cam, t_object obj, t_vector ray);
+t_object			set_cylinder(t_vector pos, t_vector dir, float r, float h);
+float				inter_cylinder(t_vector cam_pos, t_vector ray, t_object obj);
+t_vector			normal_cylinder(t_vector cam, t_object obj, t_vector ray);
 
 /*
 **	cone.c
 */
-t_object		set_cone(t_vector pos, t_vector dir, float r, float h);
-float			inter_cone(t_vector cam_pos, t_vector ray, t_object obj);
-t_vector		normal_cone(t_vector cam, t_object obj, t_vector ray);
+t_object			set_cone(t_vector pos, t_vector dir, float r, float h);
+float				inter_cone(t_vector cam_pos, t_vector ray, t_object obj);
+t_vector			normal_cone(t_vector cam, t_object obj, t_vector ray);
 
 /*
 **>----------> LIGHTS <-----
@@ -304,46 +304,46 @@ t_vector		normal_cone(t_vector cam, t_object obj, t_vector ray);
 **	light.c
 */
 
-t_vector		calc_normal(t_vector pos, t_object obj, t_vector inter);
-t_color			ft_light (t_light *ls, t_object o, t_vector inter, t_env *e);
-void			init_lights(t_env *e);
+t_vector			calc_normal(t_vector pos, t_object obj, t_vector inter);
+t_color				ft_light (t_light *ls, t_object o, t_vector inter, t_env *e);
+void				init_lights(t_env *e);
 
 /*
 **	light_diffuse.c
 */
-float			diffuse_light(t_light l, t_object o, t_vector inter, t_env *e);
+float				diffuse_light(t_light l, t_object o, t_vector inter, t_env *e);
 
 /*
 **	light_specular.c
 */
-float			specular_light(t_light l, t_object o, t_vector inter, t_env *e);
+float				specular_light(t_light l, t_object o, t_vector inter, t_env *e);
 
 /*
 **	reflection_light.c
 */
-t_color			reflection(t_light l, t_object o, t_vector inter, t_env *e);
+t_color				reflection(t_light l, t_object o, t_vector inter, t_env *e);
 
 /*
 **	ray_tracing.c
 */
-t_color			check_collision(t_env *e, t_vector ray, t_vector pos);
+t_color				check_collision(t_env *e, t_vector ray, t_vector pos);
 
 /*
 **	vector.c
 */
-t_vector		set_vector(t_vector v, float x, float y, float z);
-float			dotpro_vector(t_vector a, t_vector b);
-float			norm_vector(t_vector v);
-t_vector		unit_vector(t_vector v);
-float			sq_vector(t_vector a);
+t_vector			set_vector(t_vector v, float x, float y, float z);
+float				dotpro_vector(t_vector a, t_vector b);
+float				norm_vector(t_vector v);
+t_vector			unit_vector(t_vector v);
+float				sq_vector(t_vector a);
 
 /*
 **	vector2.c
 */
-t_vector		add_vector(t_vector v, t_vector u);
-t_vector		sub_vector(t_vector v, t_vector u);
-t_vector		cross_vector(t_vector v, t_vector u);
-t_vector		mult_vector(t_vector a, float m);
+t_vector			add_vector(t_vector v, t_vector u);
+t_vector			sub_vector(t_vector v, t_vector u);
+t_vector			cross_vector(t_vector v, t_vector u);
+t_vector			mult_vector(t_vector a, float m);
 
 /*
 **>----------> COLOR AND TEXTURE <-----
@@ -352,14 +352,14 @@ t_vector		mult_vector(t_vector a, float m);
 /*
 **	texture.c
 */
-t_color			checkered_floor(t_vector coord);
+t_color				checkered_floor(t_vector coord);
 
 /*
 **	color.c
 */
-t_color			add_color(t_color a, t_color b);
-t_color			mult_color(t_color color, float coef);
-t_color			check_color(t_color color);
+t_color				add_color(t_color a, t_color b);
+t_color				mult_color(t_color color, float coef);
+t_color				check_color(t_color color);
 
 /*
 **>----------> PARSER <-----
@@ -417,5 +417,6 @@ void				ft_keyring(t_env *e);
 void				spaw_main_menu(t_env *e);
 void				new_interface_image(t_env *e);
 void				hide_interface_image(t_env *e);
+void				spawn_cam_menu(t_env *e);
 
 #endif
--- src/ft_keyring.c
+++ src/ft_keyring.c
@@ -6,7 +6,7 @@
 /*   By: mgras <mgras@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 16:55:14 by mgras             #+#    #+#             */
-/*   Updated: 2016/02/17 13:23:14 by mgras            ###   ########.fr       */
+/*   Updated: 2016/02/17 14:12:39 by mgras            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,46 @@
 **	mode 4 = must spaw light menu (useless ?)
 **	mode 5 = light menu is up and waiting for command
 **	mode 6 = camera menu is up and waiting for command
+**	mode 7 = must spaw obj menu (useless ?)
+**	mode 8 = obj menu is up and waiting for command
 */
+
+int		ft_get_mm_cmd_interface(int x, int y, t_env *e)
+{
+	if (x >= 0 && x <= INTER_W && y >= 0 && y <= (double)INTER_H * (1. / 3.))
+		return (e->key.mode = 3);
+	if (x >= 0 && x <= INTER_W && y >= (double)INTER_H * (1. / 3.) &&
+									y <= (double)INTER_H * (2. / 3.))
+		return (e->key.mode = 4);
+	if (x >= 0 && x <= INTER_W && y >= (double)INTER_H * (2. / 3.) &&
+									y <= (double)INTER_H * (3. / 3.))
+		return (e->key.mode = 7);
+	return (e->key.mode);
+}
+
+int		ft_get_cmd_interface(int press, int x, int y, t_env *e)
+{
+	if (e->key.mode == -1 || press == 2)
+		return (-1);
+	else if (e->key.mode == 2)
+		return (ft_get_mm_cmd_interface(x, y, e));
+	/*else if (e->key.mode = 5)
+		return (ft_get_lm_cmd_interface(press, x, y, e));
+	else if (e->key.mode = 6)
+		return (ft_get_cm_cmd_interface(press, x, y, e));*/
+	return (e->key.mode);
+}
+
+int		ft_exec_cmd(int mod, t_env *e)
+{
+	if (mod == 3)
+		spawn_cam_menu(e);
+	return (0);
+}
 
 int		ft_click(int press, int x, int y, t_env *e)
 {
+	ft_exec_cmd((ft_get_cmd_interface(press, x, y, e)), e);
 	return (e->key.mode + x + y + press);
 }
 
@@ -57,16 +93,26 @@ void	hide_interface_image(t_env *e)
 	e->key.mode = -1;
 }
 
+void	set_cam_to_zero(t_cam *cam)
+{
+	t_vector	v;
+
+	cam->pos = set_vector(v, 0, 0, 0);
+	cam->dir = set_vector(v, 0, 0, 0);
+	cam->h = set_vector(v, 0, 0, 0);
+	cam->d = set_vector(v, 0, 0, 0);
+}
+
 void	init_keyring(t_env *e)
 {
 	e->key.mouse_x = 0;
 	e->key.mouse_y = 0;
-	e->key.visible = 0;
 	e->key.interface.mlx = e->mlx_init.mlx;
 	e->key.interface.win = e->mlx_init.win;
 	e->key.selected_light = 0;
 	e->key.selected_obj = 0;
 	e->key.mode = -1;
+	set_cam_to_zero(&(e->key.cam_inc));
 	new_interface_image(e);
 }
 
@@ -77,6 +123,27 @@ int		ft_mouse_move(int x, int y, t_env *e)
 	ft_putnbr(e->key.mouse_y = y);
 	ft_putchar('\n');
 	return (0);
+}
+
+void	spawn_cam_menu(t_env *e)
+{
+	t_color		c;
+	t_vector	v;
+
+	set_color_from_rgb(&c, 190, 90, 90);
+	ft_print_square(c,
+		set_vector(v, 0. , 0., 0.),
+		set_vector(v, INTER_W * (1. / 3.), (double)INTER_H * (1. / 3.), 0), e);
+	set_color_from_rgb(&c, 90, 190, 90);
+	ft_print_square(c,
+		set_vector(v, INTER_W * (1. / 3.) , 0., 0.),
+		set_vector(v, INTER_W * (2. / 3.), (double)INTER_H * (1. / 3.), 0), e);
+	set_color_from_rgb(&c, 90, 90, 190);
+	ft_print_square(c,
+		set_vector(v, INTER_W * (2. / 3.) , 0., 0.),
+		set_vector(v, INTER_W * (3. / 3.), (double)INTER_H * (1. / 3.), 0), e);
+	e->key.mode = 2;
+	mlx_put_image_to_window(e->mlx_init.mlx, e->mlx_init.win, e->key.interface.img.img_ptr, 0, 0);
 }
 
 void	spaw_main_menu(t_env *e)
