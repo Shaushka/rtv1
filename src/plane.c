6e2454fdc5deff4deafd3053645b00c70c30f275
Charles <chuang@student.42.fr>
2016-02-20 00:53:32 +0100

    carre

--- src/plane.c
+++ src/plane.c
@@ -6,7 +6,7 @@
 /*   By: mguillon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 15:57:57 by mguillon          #+#    #+#             */
-/*   Updated: 2016/02/18 16:19:43 by chuang           ###   ########.fr       */
+/*   Updated: 2016/02/20 00:52:04 by chuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,34 @@ t_object	set_plane(t_vector pos, t_vector normal)
 	return (plane);
 }
 
+
+int			defined_plane(t_vector cam_pos, t_vector ray, t_object obj)
+{
+	t_vector	dist;
+
+	dist = add_vector(cam_pos, ray);
+	printf("%f %f %f\n", fabsf(dist.x - obj.pos.x), dist.x, obj.pos.x);
+	if ((fabsf(dist.x - obj.pos.x)) < obj.height)
+		if (fabsf(dist.y - obj.pos.y) < obj.height)
+			if(fabsf(dist.z - obj.pos.z) < obj.height)
+				return (1);
+	return (0);
+}
+
 float		inter_plane(t_vector cam_pos, t_vector ray, t_object obj)
 {
 	float		t;
+	int			bound;
 
+	bound = 1;
 	t = -(dotpro_vector(obj.normal, sub_vector(cam_pos, obj.pos))
 			/ (dotpro_vector(obj.normal, ray)));
-	return (t);
+	if (t && obj.height)
+		bound = defined_plane(cam_pos, mult_vector(ray, t), obj);
+	if (bound)
+		return (t);
+	else
+		return (0);
 }
 
 t_vector	normal_plane(t_object obj, t_vector ray)
