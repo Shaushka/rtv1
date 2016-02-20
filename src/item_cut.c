4b5073f4e0f3c7071a402278f179028e6c9df3b3
Charles <chuang@student.42.fr>
2016-02-20 15:59:44 +0100

    item_cut.c

--- src/item_cut.c
+++ src/item_cut.c
@@ -4,11 +4,9 @@
 int		item_cut(t_vector pos, t_vector ray, t_object item)
 {
 	if (norm_vector(item.cut) > 0)
-
-	dotpro_vector(pos, ray);
-//	{
-//		ray = unit_vector(add_vector(pos, ray));
-//		return (dotpro_vector(ray, unit_vector(item.cut)) < 0 ? 1 : 0);
-//	}
+	{
+		pos = unit_vector(sub_vector(add_vector(ray, pos), item.pos));
+		return (dotpro_vector(pos, unit_vector(item.cut)) < 0 ? 1 : 0);
+	}
 	return (0);
 }
