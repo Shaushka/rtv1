be03e10875917200520c326cbdfb38f0f19c9e39
Charles <chuang@student.42.fr>
2016-02-20 02:23:41 +0100

    norme refraction

--- src/refraction.c
+++ src/refraction.c
@@ -12,38 +12,52 @@ float		ft_cost(float eta, float cosi)
 	return (1.0f - eta * eta * (1.0f - cosi * cosi));
 }
 
+float		inside(t_object item, t_vector inter, t_vector *norm)
+{
+	float eta;
+
+	eta = REFRACTION / item.refraction;
+	if (dotpro_vector(unit_vector(inter), *norm) > 0)
+	{
+		eta = item.refraction / REFRACTION;
+		*norm = mult_vector(*norm, -1);
+	}
+	return (eta);
+}
+
+t_vector	ft_refray(t_vector inter, t_vector norm, float eta, float cosi)
+{
+	t_vector	refray;
+
+	refray = mult_vector(unit_vector(inter), eta);
+	norm = mult_vector(norm, (eta * cosi - sqrtf(ft_cost(eta, cosi))));
+	refray = add_vector(refray, norm);
+	refray = unit_vector(refray);
+	return (refray);
+}
+
 t_color		refraction(t_object item, t_vector inter, t_env *e)
 {
-	t_vector		refray;
-	t_vector		pos;
-	t_vector		norm;
+	t_vector		joker;
 	t_vector		tmp;
 	t_color			color;
-	float			inside;
-	float			k;
 	float			eta;
 	float			cosi;
 
 	color = (t_color){0, 0, 0};
 	if (g_depth < MAX_DEPTH)
 	{
-		norm = calc_normal(e->cam.pos, item, inter);
-		inside = dotpro_vector(unit_vector(inter), norm);
-		eta = inside ? item.refraction / REFRACTION : REFRACTION / item.refraction;
-		if (inside > 0.0f)
-			norm = mult_vector(norm, -1);
-		cosi = -dotpro_vector(norm, inter);
-		(k = 1.0f - eta * eta * (1.0f - cosi * cosi));
-		{
-			g_depth++;
-			refray = add_vector(mult_vector(unit_vector(inter), eta), mult_vector(norm, (eta * cosi - sqrtf(k))));
-			refray = unit_vector(refray);
-			tmp = e->cam.pos;
-			e->cam.pos = add_vector(add_vector(inter, e->cam.pos), mult_vector(refray, 1e-4));
-			color = add_color(check_collision(e, refray, e->cam.pos), color);
-			e->cam.pos = tmp;
-			g_depth--;
-		}
+		joker = calc_normal(e->cam.pos, item, inter);
+		eta = inside(item, inter, &joker);
+		cosi = -dotpro_vector(joker, inter);
+		g_depth++;
+		joker = ft_refray(inter, joker, eta, cosi);
+		tmp = e->cam.pos;
+		e->cam.pos = add_vector(inter, e->cam.pos);
+		e->cam.pos = add_vector(e->cam.pos, mult_vector(joker, 1e-4));
+		color = add_color(check_collision(e, joker, e->cam.pos), color);
+		e->cam.pos = tmp;
+		g_depth--;
 	}
 	return (color);
 }
