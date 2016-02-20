0166835003402ff7289a722bd454aaa7d9492bc1
Marion <mguillon@student.42.fr>
2016-02-21 00:52:04 +0100

    fix du parser2

--- src/options_parser_2.c
+++ src/options_parser_2.c
@@ -6,17 +6,20 @@ void		assign_normal_p(t_object **obj, char *str)
 	int			i;
 
 	i = 0;
-	while (str[i] != '/')
+	if (obj)
+	{
+		while (str[i] != '/')
+			i++;
+		(*obj)->normal.x = ft_atoi(str);
+		while (str[i] != '/')
+			i++;
+		(*obj)->normal.y = ft_atoi(&str[i + 1]);
 		i++;
-	(*obj)->normal.x = ft_atoi(str);
-	while (str[i] != '/')
-		i++;
-	(*obj)->normal.y = ft_atoi(&str[i + 1]);
-	i++;
-	while (str[i] != '/')
-		i++;
-	(*obj)->normal.z = ft_atoi(&str[i + 1]);
-	(*obj)->normal = unit_vector((*obj)->normal);
+		while (str[i] != '/')
+			i++;
+		(*obj)->normal.z = ft_atoi(&str[i + 1]);
+		(*obj)->normal = unit_vector((*obj)->normal);
+	}
 }
 
 void		assign_dir_p(t_object **obj, char *str)
@@ -24,17 +27,20 @@ void		assign_dir_p(t_object **obj, char *str)
 	int			i;
 
 	i = 0;
-	while (str[i] != '/')
+	if (obj)
+	{
+		while (str[i] != '/')
+			i++;
+		(*obj)->dir.x = ft_atoi(str);
+		while (str[i] != '/')
+			i++;
+		(*obj)->dir.y = ft_atoi(&str[i + 1]);
 		i++;
-	(*obj)->dir.x = ft_atoi(str);
-	while (str[i] != '/')
-		i++;
-	(*obj)->dir.y = ft_atoi(&str[i + 1]);
-	i++;
-	while (str[i] != '/')
-		i++;
-	(*obj)->dir.z = ft_atoi(&str[i + 1]);
-	(*obj)->dir = unit_vector((*obj)->dir);
+		while (str[i] != '/')
+			i++;
+		(*obj)->dir.z = ft_atoi(&str[i + 1]);
+		(*obj)->dir = unit_vector((*obj)->dir);
+	}
 }
 
 void		assign_pos_p(t_object **obj, char *str)
@@ -42,14 +48,17 @@ void		assign_pos_p(t_object **obj, char *str)
 	int			i;
 
 	i = 0;
-	while (str[i] != '/')
+	if (obj)
+	{
+		while (str[i] != '/')
+			i++;
+		(*obj)->pos.x = ft_atoi(str);
+		while (str[i] != '/')
+			i++;
+		(*obj)->pos.y = ft_atoi(&str[i + 1]);
 		i++;
-	(*obj)->pos.x = ft_atoi(str);
-	while (str[i] != '/')
-		i++;
-	(*obj)->pos.y = ft_atoi(&str[i + 1]);
-	i++;
-	while (str[i] != '/')
-		i++;
-	(*obj)->pos.z = ft_atoi(&str[i + 1]);
+		while (str[i] != '/')
+			i++;
+		(*obj)->pos.z = ft_atoi(&str[i + 1]);
+	}
 }
